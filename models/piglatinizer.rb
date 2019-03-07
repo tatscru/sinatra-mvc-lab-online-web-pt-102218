df2d08b5dbecb95327145f5d0037e051ad9d7c79
tatiana.mojica <tatiana.mojica@bustle.com>
2019-03-07 23:49:22 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,29 +5,17 @@ class PigLatinizer
     words.collect {|w| piglatinize(w)}.join(" ")
   end
 
-  # def piglatinize(word)
-  #   parts_of_word = word.split(/([^aeiouAEIOU]*)([aeiouAEIOU]*)(.*)/)
-  
-  #   beginning = parts_of_word[1] 
-  #   ending = parts_of_word[2] + (parts_of_word[3] || "")
-  #   if beginning.length>0
-  #     "#{ending}#{beginning}ay"
-  #   else
-  #     "#{ending}way"
-  #   end
-  # end
-  
   def piglatinize(word)
-    return word if %w[and an in].include?(word) #one syllable exceptions
-    letters = word.split("")
-    letters.keep_if {|letter| letter != "."}
-    if letters.size > 1
-      until vowel?(letters[0]) 
-        letters << letters.shift
-      end
-      letters  << "ay"
+    parts_of_word = word.split(/([^aeiouAEIOU]*)([aeiouAEIOU]*)(.*)/)
+  
+    beginning = parts_of_word[1] 
+    ending = parts_of_word[2] + (parts_of_word[3] || "")
+    if beginning.length>0
+      "#{ending}#{beginning}ay"
+    else
+      "#{ending}ay"
     end
-    letters.join
   end
+
   
 end
